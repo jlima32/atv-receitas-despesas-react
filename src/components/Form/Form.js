853c8fd94ef9781dfcc10055eb5834720b6a6874
Juan Lima <juangustavo@gmail.com>
2024-09-09 19:07:31 -0300

    fix: label form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,19 +11,19 @@ export default function Form(){
             <form className="form">
                 <div className="form-input w100">
                     <label htmlFor="descricao">Descrição: </label>
-                    <input type="text" />
+                    <input type="text"  id="descricao"/>
                 </div>
                 <div className="form-input-group w100">
                     <div className="form-input w50">
                         <label htmlFor="tipo">Tipo: </label>
-                        <select name="tipo">
+                        <select id="tipo">
                             <option value="Despesa">Despesa</option>
                             <option value="Receita">Receita</option>
                         </select>
                     </div>
                     <div className="form-input w50">
                         <label htmlFor="valor">Valor: </label>
-                        <input type="text" />
+                        <input type="text" id="valor" />
                     </div>
                 </div>
                 <Button text="Adicionar Transação" class="btn-adicionar-transacao w100" type="submit"/>
